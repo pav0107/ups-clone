15330b6f87f5d2ed77ed2dd730e54c9c1334abce
Pav <pav0107@gmail.com>
2022-12-05 12:19:19 +0000

    set up react native navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { TailwindProvider } from 'tailwind-rn';
 import CustomersScreen from './screens/CustomersScreen';
 import utilities from './tailwind.json';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
     // @ts-ignore TailwindProvider is missing a type definition
     <TailwindProvider utilities={utilities}>
-      <CustomersScreen />
+      <NavigationContainer>
+        <CustomersScreen />
+      </NavigationContainer>
     </TailwindProvider>
   );
 }
